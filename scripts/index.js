7632aca71feae7496a8fc528bbd925ef6349c51b
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-01-23 19:14:27 +0100

    fix: js open popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 const editBtn = document.querySelector('.profile__edit-button');
-
 const popup = document.querySelector('.popup');
 const inputName = popup.querySelector('.popup__input_type_name');
 const inputInfo = popup.querySelector('.popup__input_type_info');
@@ -10,40 +9,26 @@ const profileInfo = document.querySelector('.profile__info');
 
 const saveFormBnt = popup.querySelector('.popup__container');
 
-
-editBtn.addEventListener('click', function() {
+function openPopup() {
   popup.classList.add('popup_opened');
-})
+  inputName.value = profileName.textContent;
+  inputInfo.value = profileInfo.textContent;
+}
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-closeBtn.addEventListener('click', closePopup);
-
 function formSubmitHandler(e) {
   e.preventDefault();
 
-  let name = inputName.value;
-  let info = inputInfo.value;
-
-  if(name.length !== 0 && name !== ' ') {
-    profileName.textContent = name;
-  }
-  else {
-    profileName.textContent = inputName.getAttribute('placeholder');
-    inputName.value = inputName.getAttribute('placeholder');
-  }
-
-  if(info.length !== 0 && info !== ' ') {
-    profileInfo.textContent = info;
-  }
-  else {
-    profileInfo.textContent = inputInfo.getAttribute('placeholder');
-    inputInfo.value = inputInfo.getAttribute('placeholder');
-  }
+  profileName.textContent = inputName.value;
+  profileInfo.textContent = inputInfo.value;
 
   closePopup();
 }
+
+editBtn.addEventListener('click', openPopup);
+closeBtn.addEventListener('click', closePopup);
 
 saveFormBnt.addEventListener('submit', formSubmitHandler);
