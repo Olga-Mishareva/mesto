ace63a948f923cdeb4ee82f3daa015dadd611cbf
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-01-27 21:02:07 +0100

    feat: open popup_edit-profile from js

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,34 @@
-const editBtn = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const inputName = popup.querySelector('.popup__input_type_name');
-const inputInfo = popup.querySelector('.popup__input_type_info');
-const closeBtn = popup.querySelector('.popup__close-button');
-
 const profileName = document.querySelector('.profile__name');
 const profileInfo = document.querySelector('.profile__info');
+const editBtn = document.querySelector('.profile__edit-button');
+// console.log(editBtn);
 
-const saveFormBnt = popup.querySelector('.popup__container');
+const popupTemplate = document.querySelector('#page-popup').content;
+const editProfilePopup = document.querySelector('.popup_edit-profile');
+// console.log(editProfilePopup);
+
+const profilePopup = popupTemplate.querySelector('.popup__container').cloneNode(true);
+profilePopup.querySelector('.popup__title').textContent = 'Редактировать профиль';
+profilePopup.querySelector('.popup__submit-button').textContent = 'Сохранить';
+// console.log(profilePopup.querySelector('.popup__submit-button').textContent);
+
+
+//console.log(popup);
+const inputName = profilePopup.querySelector('.popup__input_type_name');
+const inputInfo = profilePopup.querySelector('.popup__input_type_info');
+const closeBtn = profilePopup.querySelector('.popup__close-button');
+const saveFormBnt = profilePopup.querySelector('.popup__form');
+
+editProfilePopup.append(profilePopup);
 
 function openPopup() {
-  popup.classList.add('popup_opened');
+  editProfilePopup.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputInfo.value = profileInfo.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  editProfilePopup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(e) {
@@ -32,7 +44,9 @@ editBtn.addEventListener('click', openPopup);
 closeBtn.addEventListener('click', closePopup);
 
 saveFormBnt.addEventListener('submit', formSubmitHandler);
-
+// xxx.addEventListener('keyup', evt => {
+//   if(evt.code === 'Enter') closePopup();
+// });
 
 const initialCards = [
   {
@@ -64,29 +78,23 @@ const initialCards = [
 const cardTemplate = document.querySelector('#card').content;
 const cardsBox = document.querySelector('.place-grid__places');
 
-
-
 initialCards.forEach(item => {
   const initialCard = cardTemplate.querySelector('.place').cloneNode(true);
     initialCard.querySelector('.place__title').textContent = item.name;
     initialCard.querySelector('.place__image').src = item.link;
     initialCard.querySelector('.place__image').alt = item.name;
     cardsBox.append(initialCard);
-    console.log(initialCard);
+    // console.log(initialCard);
 });
 
 
 
-// function createInitialCards(elem) {
 
-//   elem.forEach(card => {
-//     const initialCard = cardTemplate.querySelector('.place').cloneNode(true);
-//     initialCard.querySelector('.place__title').textContent = card.name;
-//     initialCard.querySelector('.place__image').src = card.link;
-//     cardsBox.append(initialCard);
-//   });
-// }
 
-// createInitialCards(initialCards);
+
+
+
+
+
 
 
