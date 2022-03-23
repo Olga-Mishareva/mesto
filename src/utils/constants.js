dc56c8c3c6fffb44de146c41c0776a73a5a32581
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-03-23 19:29:38 +0100

    fix: import js-pictures for webpack

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,56 +1,64 @@
-export const initialCards = [
-  {
-    name: 'Гора Ахун',
-    link: './images/ana-kai-QXOl2IXJ_ow-unsplash.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: './images/daniil-silantev-fD7cXIFurSQ-unsplash.jpg',
-  },
-  {
-    name: 'Конжаковский Камень',
-    link: './images/daniil-silantev-hGQWGwtnbVw-unsplash.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: './images/daniil-silantev-h-M3O25tyvI-unsplash.jpg',
-  },
-  {
-    name: 'Ольхон',
-    link: './images/kir-simakov-OGc_X8PeikQ-unsplash.jpg',
-  },
-  {
-    name: 'Юрюзань',
-    link: './images/daniil-silantev-2avwToAG91M-unsplash.jpg',
-  },
-];
-
 // export const initialCards = [
 //   {
 //     name: 'Гора Ахун',
-//     link: goraAchun,
+//     link: './images/ana-kai-QXOl2IXJ_ow-unsplash.jpg',
 //   },
 //   {
 //     name: 'Камчатка',
-//     link: kamchatka,
+//     link: './images/daniil-silantev-fD7cXIFurSQ-unsplash.jpg',
 //   },
 //   {
 //     name: 'Конжаковский Камень',
-//     link: kKamen,
+//     link: './images/daniil-silantev-hGQWGwtnbVw-unsplash.jpg',
 //   },
 //   {
 //     name: 'Камчатка',
-//     link: kam_ka,
+//     link: './images/daniil-silantev-h-M3O25tyvI-unsplash.jpg',
 //   },
 //   {
 //     name: 'Ольхон',
-//     link: olchon,
+//     link: './images/kir-simakov-OGc_X8PeikQ-unsplash.jpg',
 //   },
 //   {
 //     name: 'Юрюзань',
-//     link: urusan,
+//     link: './images/daniil-silantev-2avwToAG91M-unsplash.jpg',
 //   },
 // ];
+
+export const goraAchun = new URL('../images/ana-kai-QXOl2IXJ_ow-unsplash.jpg', import.meta.url);
+export const kamchatka = new URL('../images/daniil-silantev-fD7cXIFurSQ-unsplash.jpg', import.meta.url);
+export const kKamen = new URL('../images/daniil-silantev-hGQWGwtnbVw-unsplash.jpg', import.meta.url);
+export const kam_ka = new URL('../images/daniil-silantev-h-M3O25tyvI-unsplash.jpg', import.meta.url);
+export const olchon = new URL('../images/kir-simakov-OGc_X8PeikQ-unsplash.jpg', import.meta.url);
+export const urusan = new URL('../images/daniil-silantev-2avwToAG91M-unsplash.jpg', import.meta.url);
+
+
+export const initialCards = [
+  {
+    name: 'Гора Ахун',
+    link: goraAchun,
+  },
+  {
+    name: 'Камчатка',
+    link: kamchatka,
+  },
+  {
+    name: 'Конжаковский Камень',
+    link: kKamen,
+  },
+  {
+    name: 'Камчатка',
+    link: kam_ka,
+  },
+  {
+    name: 'Ольхон',
+    link: olchon,
+  },
+  {
+    name: 'Юрюзань',
+    link: urusan,
+  },
+];
 
 export const profileData = {
   nameSelector: '.profile__name',
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require('path'); // подключаем path к конфигу вебпак
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключение плагина
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' }, // указали первое место, куда заглянет webpack, — файл index.js в папке src
-  output: {                                  // указали в какой файл будет собираться весь js и дали ему имя
-    path: path.resolve(__dirname, './dist'),  // переписали точку выхода, используя утилиту path
-    filename: 'main.js',  // файл с результатом сборки js-файлов
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
     publicPath: ''
   },
-  mode: 'development',  // добавили режим разработчика
+  mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-    compress: true,   // это ускорит загрузку в режиме разработки
-    port: 8800,      // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-    open: true      // сайт будет открываться сам при запуске npm run dev
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8800,
+    open: true
   },
 
   module: {
-    rules: [ // rules — это массив правил. добавим в него объект правил для бабеля
+    rules: [
     {
-      test: /\.js$/,           // регулярное выражение, которое ищет все js файлы
-      use: 'babel-loader',       // при обработке этих файлов нужно использовать babel-loader
-      exclude: '/node_modules/',   // исключает папку node_modules, файлы в ней обрабатывать не нужно
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: '/node_modules/'
     },
     {
-      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/, // регулярное выражение, которое ищет все файлы с такими расширениями
+      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
       type: 'asset/resource'
     },
     {
@@ -39,11 +39,11 @@ module.exports = {
   },
 
   plugins: [
-    new HtmlWebpackPlugin({          // использование плагина
+    new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin()
   ]
 };
 
