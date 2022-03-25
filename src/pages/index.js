6bbd5d3f54881d1f6af0b6262f575a07b2a12885
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-03-25 18:09:11 +0100

    fix: separete create card function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,114 +1,112 @@
-import './index.css';
+import "./index.css";
 
-import FormValidator from '../components/FormValidator.js';
-import Card from '../components/Card.js';
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import UserInfo from '../components/UserInfo.js';
-import { initialCards,
+import FormValidator from "../components/FormValidator.js";
+import Card from "../components/Card.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
+import {
+  initialCards,
   profileData,
   settings,
   formValidators,
-  editBtn,
+  profileBtn,
   inputName,
   inputInfo,
-  addBtn,
+  placeBtn,
   inputCard,
-  inputLink
-} from '../utils/constants.js';
-
+  inputLink,
+} from "../utils/constants.js";
 
 // --------------------------------------------------------------------------
 
-const formList = Array.from(document.querySelectorAll('.popup__form'));
+const formList = Array.from(document.querySelectorAll(".popup__form"));
 formList.forEach((formElement) => {
   const validator = new FormValidator(settings, formElement);
-  const formName = formElement.getAttribute('name');
+  const formName = formElement.getAttribute("name");
   formValidators[formName] = validator;
   validator.enableValidation();
 });
-
 
 // ------ popup_edit-profile -----------------------------------------------
 
 // создает класс вставки в DOM
 const userData = new UserInfo({ data: profileData });
 
-const profilePopup = new PopupWithForm({
-  handleSubmit: (data) => {
-    userData.setUserInfo(data);
-  }
-}, '.popup_type_edit-profile');
+const profilePopup = new PopupWithForm(
+  {
+    handleSubmit: (data) => {
+      userData.setUserInfo(data);
+    },
+  },
+  ".popup_type_edit-profile"
+);
 
 profilePopup.setEventListeners();
 
-
 // кнопка открытия ред.профиля
-editBtn.addEventListener('click', function () {
-
-  inputName.value = userData.getUserInfo().username;
-  inputInfo.value = userData.getUserInfo().about;
+profileBtn.addEventListener("click", function () {
+  const userIntel = userData.getUserInfo();
+  inputName.value = userIntel.username;
+  inputInfo.value = userIntel.about;
 
   profilePopup.openPopup();
 
-  formValidators['profile-form'].resetValidation();
+  formValidators["profile-form"].resetValidation();
 });
-
 
 // ------ popup_add-place -----------------------------------------------
 
+// созжание катрочки
+function createCard(item) {
+  const card = new Card(item, "#card", handleCardClick);
+  const cardElement = card.generateCard();
+
+  return cardElement;
+}
 
 // отрисовка начального массива карточек
-const cardsGrid = new Section({
-  items: initialCards,
-  renderer: (item) => {
-    const card = new Card(item, '#card', handleCardClick);
-    const cardElement = card.generateCard();
-    cardsGrid.addItem(cardElement);
-  }
-}, '.place-grid__places');
+const cardsGrid = new Section(
+  {
+    items: initialCards,
+    renderer: (item) => {
+      cardsGrid.addItem(createCard(item));
+    },
+  },
+  ".place-grid__places"
+);
 
-// создание новой карточки
-const placePopup = new PopupWithForm({
-  handleSubmit: (elem) => {
-    const data = {};
-    data.name = elem[inputCard.name];
-    data.link = elem[inputLink.name];
+// сохранеие и вставка новой карточки
+const placePopup = new PopupWithForm(
+  {
+    handleSubmit: (elem) => {
+      const data = {};
+      data.name = elem[inputCard.name];
+      data.link = elem[inputLink.name];
 
-    const newCard = new Card(data, '#card', handleCardClick);
-    const newCardElement = newCard.generateCard();
-    cardsGrid.addItem(newCardElement);
-  }
-},'.popup_type_add-place');
+      cardsGrid.addItem(createCard(data));
+    },
+  },
+  ".popup_type_add-place"
+);
 
 placePopup.setEventListeners();
 
 cardsGrid.renderItems();
 
-
 // кнопка добавить карточку
-addBtn.addEventListener('click', () => {
+placeBtn.addEventListener("click", () => {
   placePopup.openPopup();
-  formValidators['add-form'].resetValidation();
+  formValidators["add-form"].resetValidation();
 });
 
 // ------ popup_show-image -----------------------------------------------
 
-const popupWihtImage = new PopupWithImage('.popup_type_show-image');
-
+const popupWihtImage = new PopupWithImage(".popup_type_show-image");
 
 // показ картинки
 function handleCardClick(name, link) {
   popupWihtImage.openPopup(name, link);
   popupWihtImage.setEventListeners();
 }
-
-
-
-
-
-
-
-
-
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,30 +1,3 @@
-// export const initialCards = [
-//   {
-//     name: 'Гора Ахун',
-//     link: './images/ana-kai-QXOl2IXJ_ow-unsplash.jpg',
-//   },
-//   {
-//     name: 'Камчатка',
-//     link: './images/daniil-silantev-fD7cXIFurSQ-unsplash.jpg',
-//   },
-//   {
-//     name: 'Конжаковский Камень',
-//     link: './images/daniil-silantev-hGQWGwtnbVw-unsplash.jpg',
-//   },
-//   {
-//     name: 'Камчатка',
-//     link: './images/daniil-silantev-h-M3O25tyvI-unsplash.jpg',
-//   },
-//   {
-//     name: 'Ольхон',
-//     link: './images/kir-simakov-OGc_X8PeikQ-unsplash.jpg',
-//   },
-//   {
-//     name: 'Юрюзань',
-//     link: './images/daniil-silantev-2avwToAG91M-unsplash.jpg',
-//   },
-// ];
-
 export const goraAchun = new URL('../images/ana-kai-QXOl2IXJ_ow-unsplash.jpg', import.meta.url);
 export const kamchatka = new URL('../images/daniil-silantev-fD7cXIFurSQ-unsplash.jpg', import.meta.url);
 export const kKamen = new URL('../images/daniil-silantev-hGQWGwtnbVw-unsplash.jpg', import.meta.url);
@@ -80,13 +53,13 @@ export const formValidators = {};
 
 // ----------------------------------------------------------------
 
-export const editBtn = document.querySelector('.profile__edit-button');
+export const profileBtn = document.querySelector('.profile__edit-button');
 export const inputName = document.querySelector('.popup__input_type_name');
 export const inputInfo = document.querySelector('.popup__input_type_info');
 
 // --------------------------------------------------------------
 
-export const addBtn = document.querySelector('.profile__add-button');
+export const placeBtn = document.querySelector('.profile__add-button');
 export const inputCard = document.querySelector('.popup__input_type_place');
 export const inputLink = document.querySelector('.popup__input_type_link');
 
